4b7599d97180205dfa3d3712a7ab818e512bc590
jcgsr <cabralguerra@gmail.com>
2021-04-11 09:38:04 -0300

    navbar fix

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,68 +1,61 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <div class="contents">
-        <router-link to="/" class="navbar-brand">
-          <img id="sign" :src="require('../assets/signature.jpeg')" />
-        </router-link>
+    <router-link to="/" class="navbar-brand">
+      <img id="sign" :src="require('../assets/signature.jpeg')" />
+    </router-link>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
 
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/acolhimento">ACOLHIMENTO</router-link>
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/avaliacao">AVALIAÇÃO</router-link>
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/intervencao"
-                  >INTERVENÇÃO ABA INTENSIVA</router-link
-                >
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/treinamento"
-                  >TREINAMENTO PARENTAL</router-link
-                >
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/supervisao"
-                  >SUPERVISÃO PROFISSIONAL</router-link
-                >
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/cursos">CURSOS</router-link>
-              </button>
-            </li>
-            <li class="nav-item">
-              <button type="button" class="btn btn-light">
-                <router-link to="/fale">FALE CONOSCO</router-link>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <button type="button" class="btn btn-light">
+            <router-link to="/acolhimento">ACOLHIMENTO</router-link>
+          </button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-light">
+            <router-link to="/avaliacao">AVALIAÇÃO</router-link>
+          </button>
+        </li>
+        <li class="nav-item dropdown">
+          <button type="button" class="btn btn-light">
+            <router-link to="/intervencao"
+              >INTERVENÇÃO ABA INTENSIVA</router-link
+            >
+          </button>
+        </li>
+        <li class="nav-item">
+          <button type="button" class="btn btn-light">
+            <router-link to="/treinamento">TREINAMENTO PARENTAL</router-link>
+          </button>
+        </li>
+
+        <li>
+          <button type="button" class="btn btn-light">
+            <router-link to="/supervisao">SUPERVISÃO PROFISSIONAL</router-link>
+          </button>
+        </li>
+        <li>
+          <button type="button" class="btn btn-light">
+            <router-link to="/cursos">CURSOS</router-link>
+          </button>
+        </li>
+        <li>
+          <button type="button" class="btn btn-light">
+            <router-link to="/fale">FALE CONOSCO</router-link>
+          </button>
+        </li>
+      </ul>
     </div>
   </nav>
 </template>
@@ -85,10 +78,15 @@ img#sign {
 .navbar li a {
   color: #869bac;
   text-decoration: none;
-}
-
-.contents {
   display: flex;
-  align-items: center;
+}
+ul li {
+  text-align: left;
+}
+@media screen and (max-width: 456px) {
+  img#sign {
+    width: 250px;
+    height: 180px;
+  }
 }
 </style>
