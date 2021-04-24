e219bccbb7719bc3cb2cbcccd7e89a58c7421785
jcgsr <cabralguerra@gmail.com>
2021-04-24 09:09:03 -0300

    logo bg remove

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <router-link to="/" class="navbar-brand">
-      <img id="sign" :src="require('../assets/signature_blank.jpg')" />
+      <img id="sign" :src="require('../assets/signature1remove.png')" />
     </router-link>
     <button
       class="navbar-toggler"
