0ddf4e376b24db0f3dbb05f9fcf4415629a13b8a
jcgsr <cabralguerra@gmail.com>
2021-04-30 14:46:58 -0300

    img cover bug

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,20 +1,31 @@
 <template>
-  <div>
-    <img :src="require('../assets/capa.jpg')" alt="Camylle" />
-  </div>
+	<div class="home">
+		<img :src="require('../assets/capa.jpg')" alt="Camylle" />
+	</div>
 </template>
 
 <script>
-export default {
-  name: "Home",
-};
+	export default {
+		name: "Home",
+	};
 </script>
 
 <style>
+.home {
+	width: 100%;
+	margin-top: 7rem;
+}
 h1 {
-  text-align: center;
+	text-align: center;
 }
-img {
-  width: 100%;
+img{
+	/*width: 100%;
+	background-size: cover;*/
 }
+/*
+@media screen and (max-width: 456px) {
+	img {
+		height: 80vh;
+	}
+}*/
 </style>
--- src/components/Supervisao.vue
+++ src/components/Supervisao.vue
@@ -22,7 +22,7 @@
 
 <script>
 export default {
-  name: "Supervisão Profissional",
+  name: "SupervisaoProfissional",
   components: {},
 };
 </script>
